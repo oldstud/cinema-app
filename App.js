00f29b73b61172d1451af77034d417a4d54f6233
Dima <respect4me@ukr.net>
2021-09-26 14:40:43 +0300

    init project

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+
+const App: () => Node = () => {
+
+
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+     <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+
+});
+
+export default App;
